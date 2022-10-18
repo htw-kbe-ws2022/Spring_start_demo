fb1850bd4096debafe7bf3aeecdf4f446db2444c
s0575347 <johannes.plath@student.htw-berlin.de>
2022-10-18 18:13:30 +0200

    init Version2

---
+++ src/test/java/com/example/kbe/spring/demo/start/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kbe.spring.demo.start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
